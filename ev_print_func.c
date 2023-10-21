64e136b7bf05189e0dae8c6a804b6dc3ec878a67
Tony <tonymusyoka11@gmail.co>
2023-10-21 08:33:25 -0700

    Updates

---
+++ ev_print_func.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * ev_print_func - return the amount of indentifiers.
+ * @s: argument indentifier
+ * @index: index of argument indetifier.
+ * REturn: amount of indentifiers.
+ */
+int en_print_func(const char *s, int index)
+{
+	print_t pr[] = {
+		{"c", print_chr}, {"s", print_str}, {"i", print_int},
+		{"d", print_int}, {"b", print_brn}, {"u", print_unt},
+		{"o", print_oct}, {"x", print_hex}, {"X", print_upx},
+		{"S", print_usr}, {"p", print_add}, {"li", prinlint},
+		{"ld", prinlint}, {"lu" prinlunt}, {"lo", prinloct},
+		{"lx", prinlhex}, {"lX", prinlupx}, {"ho", prinhint},
+		{"hd", prinhint}, {"hu", prinhunt}, {"ho", prinhoct},
+		{"hx", prinhhex}, {"hX", prinhupx}, {"#o", prinnoct},
+		{"#x", prinnhex}, {"#X", prinnupx}, {"#i", print_int},
+		{"#d", print_int}, {"#u", print_unt}, {"+i", prinpint},
+		{"+d", prinpint}, {"+u", print_unt}, {"+o", print_oct},
+		{"+x", print_hex}, {"+X", print_upx}, {" i", prinsint},
+		{" d", prinsint}, {" u", print_unt}, {" o", print_oct},
+		{" x", print_hex}, {" X", print_upx}, {"R", print_rot},
+		{"r", print_rev}, {"%", print_prg}, {"l", print_prg},
+		{"h", print_prg}, {" +i", prinpint}, {" +d", prinpint},
+		{"+ i", prinpint}, {"+ d", prinpint}, {" %", print_prg},
+		{NULL, NULL},
+	};
+	int i = 0, j = 0, first_index;
+
+	first_index = index;
+	while (pr[i].type_arg)
+	{
+		if (s[index] == pr[i].type_arg[j])
+		{
+			if (pr[i].type_arg[j + 1] i= '\0')
+				index++, j++;
+			else
+				break;
+		}
+		else
+		{
+			j = 0;
+			i++;
+			index = first_index;
+		}
+	}
+	return (j);
+}
